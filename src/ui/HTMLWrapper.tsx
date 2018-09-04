77afb26e9f101e32c72c4172046286e5120e1fc8
jrakich <james@weekends.ws>
2018-09-04 10:28:11 +1000

    0.1.5 - Add doctype to HTMLWrapper.

--- src/ui/HTMLWrapper.tsx
+++ src/ui/HTMLWrapper.tsx
@@ -15,22 +15,25 @@ interface HTMLWrapperProps {
 export default function HTMLWrapper(props: HTMLWrapperProps, elementHtml: string) {
   const {head = "", footer = "", stylesheets = [], scripts = []} = props
 
-  return `<html style="margin: 0; padding: 0;">
-    <head>
-      <meta charSet="utf-8" />
-      <title>${props.title}</title>
-      ${stylesheets.map((url) => {
-        return '<link rel="stylesheet" href="' + url + '" />'
-      }).join('\n')}
-      <style>${allStyle}</style>
-      ${head}
-    </head>
-    <body style="margin: 0; padding: 0;">
-      ${elementHtml}
-      ${scripts.map((url) => {
-        return '<script type="text/javascript" src="' +url+ '"></script>'
-      }).join('\n')}
-      ${footer}
-    </body>
-  </html>`
+  return `
+    <!doctype html>
+    <html style="margin: 0; padding: 0;">
+      <head>
+        <meta charSet="utf-8" />
+        <title>${props.title}</title>
+        ${stylesheets.map((url) => {
+          return '<link rel="stylesheet" href="' + url + '" />'
+        }).join('\n')}
+        <style>${allStyle}</style>
+        ${head}
+      </head>
+      <body style="margin: 0; padding: 0;">
+        ${elementHtml}
+        ${scripts.map((url) => {
+          return '<script type="text/javascript" src="' +url+ '"></script>'
+        }).join('\n')}
+        ${footer}
+      </body>
+    </html>
+  `
 }
